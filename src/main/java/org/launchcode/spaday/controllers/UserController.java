b738b448e1b1661893cf5bd86ecb6b5e9f567534
unknown <nunezfrancisco400@gmail.com>
2022-08-31 19:22:03 -0400

    in processss

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,8 +1,33 @@
 package org.launchcode.spaday.controllers;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.launchcode.spaday.models.User;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("user")
 public class UserController {
+
+
+    @GetMapping("add")
+    @ResponseBody
+    public String displayAddUserForm(){
+        return "/user/add";
+    }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify){
+        model.addAttribute("user",user);
+        model.addAttribute("verify",verify);
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email",user.getEmail());
+        if(user.getPassword().equals(verify)){
+            return "user/index";
+        }else{
+            model.addAttribute("error", "Passwords do not match");
+            return "user/add";
+        }
+
+
+    }
 }
